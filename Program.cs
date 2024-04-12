7da7fd61be94e31874c94c3bc33361a0b4d6da7e
Tika Khadka <tikakhadka3132@gmail.com>
2024-04-12 09:10:46 +0545

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Animal A1=new();
+Animal A2=new();
+
+
+A1.species="Cat";
+A1.breed="Birman";
+A1.weight=4;
+A1.limbs=4;
+A1.color="white";
+A1.PrintDetail();
+
+A2.species="Dog";
+A1.breed="pomeranian";
+A2.weight=2;
+A2.limbs=4;
+A2.color="brown"; 
+A2.PrintDetail();
